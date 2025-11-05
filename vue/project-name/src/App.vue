93bb8acb8b4dfee4573dedc4a802cb5872af45a3
lfw529 <lifuwen@leoao.com>
2025-11-05 18:24:30 +0800

    add 1105

--- vue/project-name/src/App.vue
+++ vue/project-name/src/App.vue
@@ -1,25 +1,56 @@
 <template>
   <div>
-    <BaseSelect :value="selectId" @input="selectId = $event"></BaseSelect>
+    <MyTable :data="list">
+      <template #default="obj">
+        <button @click="del(obj.row.id)">
+          删除
+        </button>
+      </template>
+    </MyTable>
+
+    <MyTable :data="list2">
+      <template #default="{ row }">
+        <button @click="show(row)">
+          查看
+        </button>
+      </template>
+    </MyTable>
   </div>
 </template>
 
 <script>
-import BaseSelect from './components/BaseSelect.vue'
-
+import MyTable from './components/MyTable.vue'
 export default {
-  data() {
+  data () {
     return {
-      selectId: '102',
+      list:[
+        {id:1, name:'张三', age:19},
+        {id:2, name:'李四', age:20},
+        {id:3, name:'王五', age:21},
+      ],
+      list2:[
+        {id:1, name:'张三', age:19},
+        {id:2, name:'李四', age:20},
+        {id:3, name:'王五', age:21},
+      ],
+    }
+  },
+  methods: {
+    del(id) {
+      this.list = this.list.filter(item => item.id !== id)
+    },
+    show(row) {
+      alert(`姓名：${row.name}，年龄：${row.age}`)
     }
   },
   components: {
-    BaseSelect,
-  },
+    MyTable
+  }
 }
-      
-
 </script>
 
 <style>
+body {
+  background-color: #b3b3b3;
+}
 </style>
